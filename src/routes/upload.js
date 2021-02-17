85b201a218b27b530a5c9d07cbff82b4e80b3271
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-17 04:24:35 -0500

    Update upload.js

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -33,17 +33,12 @@ router.post('/upload', async function(req, res) {
             res.json({error: `Not using Sharex Uploader`})
             return;
           }
-            
             s3A.uploadImage(data[0].id, files.cyci.name, files.cyci.path).then((file) => {
               logger.log(`${file} uploaded by ${ip} - ${who}`);
               let checkIfImg = JSON.parse(data[0].fileLink);
               if (!checkIfImg.includes(file)) {
                 db.query(`UPDATE userData SET fileLink=COALESCE(JSON_ARRAY_APPEND(fileLink, '$', '${file}'), JSON_ARRAY('${file}'))`);
-                res.json({url: "https://" + file});
                 return;
-              }
-              else if (checkIfImg.includes(file)) {
-              return res.json({url: "https://" + file});
               }
             }).catch((err) => {
               logger.error(err);
