1464c898ed379eef7e3917f5e865e2b4a22eec36
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-02-17 02:15:58 -0500

    Update upload.js

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -38,12 +38,14 @@ router.post('/upload', async function(req, res) {
             s3A.uploadImage(data[0].id, files.cyci.name, files.cyci.path).then((file) => {
               logger.log(`${file} uploaded by ${ip} - ${who}`);
               let checkIfImg = JSON.parse(data[0].fileLink);
-              if (checkIfImg.includes(file)) return res.json({url: "https://" + file});
-              else if (checkIfImg.includes(file) == null) {
-              db.query(`UPDATE userData SET fileLink=COALESCE(JSON_ARRAY_APPEND(fileLink, '$', '${file}'), JSON_ARRAY('${file}'))`);
-              res.json({url: "https://" + file});
-              res.end();
-              return;
+              if (checkIfImg.includes(file) == null) {
+                db.query(`UPDATE userData SET fileLink=COALESCE(JSON_ARRAY_APPEND(fileLink, '$', '${file}'), JSON_ARRAY('${file}'))`);
+                res.json({url: "https://" + file});
+                res.end();
+                return;
+              }
+              else if (checkIfImg.includes(file)) {
+              return res.json({url: "https://" + file});
               }
             }).catch((err) => {
               logger.error(err);
