415f4d850270cb28c517727092d79160900bf017
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-03-03 20:19:21 -0500

    Update link.js

--- src/routes/link.js
+++ src/routes/link.js
@@ -19,7 +19,9 @@ async function get(req, res, next) {
         res.status(301).redirect(entireLink.shorten)
         const dayjs = require('dayjs')
         const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
-        db.query(`UPDATE userData SET visits=JSON_ARRAY_APPEND(visits, '$.${timestamp}', '{'${timestamp}': {'${ip}': {'who: {${who}}}}}') WHERE id="${entireLink.id}"`)
+        db.query(`UPDATE userData SET visits=JSON_ARRAY_APPEND(visits, '$.${timestamp}', '{'${timestamp}': {'${ip}': {'who: {${who}}}}}') WHERE id="${entireLink.id}"`).om('error', (err)=> {
+            console.log(err);
+        })
     })
 }
 
