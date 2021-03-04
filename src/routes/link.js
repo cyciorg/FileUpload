d49d5f8898105eb4bd9b9091bb05f7f62e8fdd25
Hayden <haydoprice@gmail.com>
2021-03-03 23:03:52 -0600

    Update link.js
    burger

--- src/routes/link.js
+++ src/routes/link.js
@@ -3,14 +3,6 @@ const createID = require('../utils/createID');
 const logger = require('../utils/logger');
 const renderTemplate = require('../utils/renderFile');
 async function get(req, res, next) {
-    /**
-     <html><head><meta property='og:site_name' content='CyciUploader'>
-    <meta property='og:description' content='Cyci's personal fileuploader'>
-    <meta name='theme-color' content='#7289DA'>
-    <meta property='og:type'content='website'>
-    <meta property='og:title' content='Cyci Upload' />
-    <meta property='og:image' content='https://cdn.cyci.rocks/576688747481743/user-1.png' >
-    <link rel='icon' href='https://cdn.cyci.rocks/576688747481743/index.svg'> </head></html>*/
     res.set("Content-Type", "text/html;");
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress,who = req.headers['user-agent'] || "Undefined (1.0.0)";
     if (!req.params) return res.status(403).json({ error: 'Choose a correct path' });
