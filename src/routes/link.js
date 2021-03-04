ecf5ecf93745b18f85633a705b6f7f533979182b
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-03-03 20:24:54 -0500

    Update link.js

--- src/routes/link.js
+++ src/routes/link.js
@@ -1,4 +1,5 @@
 const db = require('../database/mysql');
+const createID = require('../utils/createID');
 const logger = require('../utils/logger');
 const renderTemplate = require('../utils/renderFile');
 async function get(req, res, next) {
@@ -20,7 +21,7 @@ async function get(req, res, next) {
         res.status(301).redirect(entireLink.shorten)
         const dayjs = require('dayjs')
         const timestamp = dayjs(new Date()).format("YYYY,MM,DD");
-        db.query(`UPDATE userData SET visits=JSON_ARRAY_APPEND(visits, '$.${timestamp}', '{'${timestamp}': {'${ip}': {'who: {${who}}}}}') WHERE id="${entireLink.id}"`).on('error', (err)=> {
+        db.query(`UPDATE userData SET visits=JSON_ARRAY_APPEND(visits, '$.${timestamp}', '{'${timestamp}': {'${ip}': {'who': {'${createID(20)}: "${who}"}}}}') WHERE id="${entireLink.id}"`).on('error', (err)=> {
             console.log(err);
         })
     })
