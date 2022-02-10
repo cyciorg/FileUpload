9b641890ab75cd3f27d39c123dc1a4ac4cf1fb6c
Phil - DopeDealers <dopedealersdev@gmail.com>
2022-02-09 23:23:58 -0500

    MMM add promises to findByEmail

--- src/db/User.schema.js
+++ src/db/User.schema.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 var dayjs = require('dayjs');
 const roles = require('../utils/roles');
+const crypto = require('crypto');
 var now = dayjs()
 
 const UserAccount = new Schema({
@@ -10,26 +11,100 @@ const UserAccount = new Schema({
   email: String,
   api_token: String,
   roles: [Number],
-  data: [{name: String, value: String, size: String, type: String, created_at: Date}],
+  data: [{name: String, id: String, value: String, size: String, type: String, created_at: Date}],
   account_type: String,
   date: Date
 });
 
-UserAccount.statics.findByEmailOrId = function findByEmailOrId(data, cb){
+UserAccount.statics.findByEmailOrId = async function findByEmailOrId(data, cb){
   if (!data) return cb(new Error('No data provided'));
   if (data.email) {
-    this.findOne({email: data.email}, function(err, result){
-      if (err) return cb(err);
-      if (!result) return cb(null, false);
-      return cb(null, result);
-    });
+    let account = await this.findOne({email: data.email}).then();
+    if (account.err) return new Promise((resolve, reject) => {reject(account.err)});
+    if (!account) return new Promise((resolve, reject) => {reject(new Error('No account found'));});
+    return new Promise((resolve, reject) => {resolve(account)});
   } else if (data.userid) {
-    this.findOne({userid: data.userid}, function(err, result){  
+    let account = await this.findOne({email: data.email}).then();
+    if (account.err) return new Promise((resolve, reject) => {reject(account.err)}); 
+    if (!account) return new Promise.reject(new Error('No account found'));
+    return new Promise((resolve, reject) => {resolve(account)});
+  }
+};
+
+UserAccount.statics.removeImageOrFile = function removeImageOrFile(user, data, cb){
+  if (!user) return cb(new Error('No user provided'));
+  if (!data) return cb(new Error('No data provided'));
+  if (data.name) {
+    this.findOne({userid: user.userid}, function(err, result){
       if (err) return cb(err);
       if (!result) return cb(null, false);
-      return cb(null, result);
+      for (let i = 0; i < result.data.length; i++) {
+        if (result.data[i].name == data.name) {
+          result.data.splice(i, 1);
+          result.save(function(err, result){
+            if (err) return cb(err);
+            return cb(null, result);
+          });
+        }
+      }
+    });
+  } else if (data.id) {
+    this.findOne({userid: user.userid}, function(err, result){
+      if (err) return cb(err);
+      if (!result) return cb(null, false);
+      for (let i = 0; i < result.data.length; i++) {
+        if (result.data[i]._id == data.id) {
+          result.data.splice(i, 1);
+          result.save(function(err, result){
+            if (err) return cb(err);
+            return cb(null, result);
+          });
+        }
+      }
     });
   }
+};
+
+UserAccount.statics.removeRole = function removeRole(user, role, cb){
+  if (!user) return cb(new Error('No user provided'));
+  if (!role) return cb(new Error('No role provided'));
+  this.findOne({userid: user.userid}, function(err, result){
+    if (err) return cb(err);
+    if (!result) return cb(null, false);
+    for (let i = 0; i < result.roles.length; i++) {
+      if (result.roles[i] == role) {
+        result.roles.splice(i, 1);
+        result.save(function(err, result){
+          if (err) return cb(err);
+          return cb(null, result);
+        });
+      }
+    }
+  });
+};
+
+UserAccount.statics.getUser = function getUser(user, cb){
+  if (!user) return cb(new Error('No user provided'));
+  this.findOne({userid: user.userid}, function(err, result){
+    if (err) return cb(err);
+    if (!result) return cb(null, false);
+    return cb(null, result);
+  });
+};
+
+// Same as AppendRole, but for convenience its added as addRole
+UserAccount.statics.addRole = function addRole(user, role, cb){
+  if (!user) return cb(new Error('No user provided'));
+  if (!role) return cb(new Error('No role provided'));
+  this.findOne({userid: user.userid}, function(err, result){
+    if (err) return cb(err);
+    if (!result) return cb(null, false);
+    result.roles.push(role);
+    result.save(function(err, result){
+      if (err) return cb(err);
+      return cb(null, result);
+    });
+  });
 };
 
 
@@ -40,11 +115,30 @@ UserAccount.statics.appendRole = function appendRole(user, role, cb){
   this.findOne({userid: user.userid}, function(err, result){  
     if (err) return cb(err);
     if (user.roles.includes(role)) return cb(new Error('User already has that role'));
+    if (!result) return cb(null, false);
     user.roles.push(role);
     user.save(function(err, result){
       if (err) return cb(err);
       return cb(null, result);
     });
+  });
+};
+
+UserAccount.statics.generateApiToken = function generateApiToken(user, cb){
+  if (!user) return cb(new Error('No user provided'));
+  user.api_token = crypto.randomBytes(32).toString('hex');
+  user.save(function(err, result){
+    if (err) return cb(err);
+    return cb(null, result);
+  });
+};
+
+UserAccount.statics.getImagesOrFiles = function getImagesOrFiles(user, cb){
+  if (!user) return cb(new Error('No user provided'));
+  this.findOne({userid: user.userid}, function(err, result){
+    if (err) return cb(err);
+    if (!result) return cb(null, false);
+    return cb(null, result.data);
   });
 };
 
@@ -84,14 +178,6 @@ UserAccount.statics.findOrCreate = function findOrCreate(profile, cb){
           if (err) return cb(err);
           return cb(null, result);
         });
-          // userObj.email = profile.email;
-          // userObj.account_type = profile.premium_type;
-          // userObj.userid = profile.id;
-          // userObj.api_token = null;
-          // userObj.roles = [];
-          // userObj.data = [];
-          // userObj.date = now;
-          // userObj.save(cb);
       }else{
           cb(err,result);
       }
--- src/routes/upload.js
+++ src/routes/upload.js
@@ -5,28 +5,66 @@ const formidable = require('formidable');
 const fileSettings = require('../utils/fileSettings');
 const createID = require('../utils/createID');
 const {models} = require('../db/connector');
+const mimeType = require('mime-types');
+const { log } = require('winston');
+
 
 const s3A = new AmazonCDN();
 
 async function post(req, res) {
     res.setHeader('Content-Type', 'text/text');
-    console.log(req);
+    
     let form = new formidable.IncomingForm();
-    let userHeaderInformation = req.headers,
-    userEmail = userHeaderInformation.xuser-email,
-    userID = userHeaderInformation.xuser-id,
-    userToken = userHeaderInformation.xuser-api_token;
+    let userHeaderInformation = req.headers;
 
     let fileName = createID(10);
 
-    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress,who = req.headers['user-agent'] || "Undefined (1.0.0)";
-    if (!userEmail || !userID && !userToken) return res.json({error: `Unauthorized request`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`), 
-    let account = await models.UserAccount.findByEmailOrId({email: userEmail, userid: userID});
+    //let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.realAddress || req.connection.remoteAddress,who = req.headers['user-agent'] || "Undefined (1.0.0)";
+    if (!userHeaderInformation['xuser-email'] || !userHeaderInformation['xuser-id'] && !userHeaderInformation['xuser-api_token']) return res.json({error: `Unauthorized request`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`), 
+    console.log(userHeaderInformation['xuser-id'] + " " + userHeaderInformation['xuser-email']);;
+    let account = await models.User.findByEmailOrId({email: userHeaderInformation['xuser-email'], userid: userHeaderInformation['xuser-id']}).then();
+    
+    
     if (!account) return res.json({error: `Unauthorized request. user does not exist.`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
     if (!account.api_token) return res.json({error: `Unauthorized request. user does not have an api token.`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
-    if (account.api_token !== userToken) return res.json({error: `Unauthorized request. user api token does not match.`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
-    
-    
+    if (account.api_token !== userHeaderInformation['xuser-api_token']) return res.json({error: `Unauthorized request. user api token does not match.`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
+
+
+    form.parse(req, async (err, fields, files) => {
+        if (err) return res.json({error: `Error parsing form.`});
+        if (!files.cyciUploader) return res.json({error: `No file provided.`});
+        if (!files.cyciUploader.size) return res.json({error: `File is empty.`});
+        if (files.cyciUploader.size > fileSettings.maxFileSize) return res.json({error: `File is too large.`});
+        if (files.cyciUploader && !fields.key) {
+            files.cyci = files.cyciUploader;
+        } else {
+            //logger.error(`Non sharex upload requested by ${ip} - ${data[0].name}/${who}`)
+            //eturn res.json({error: `Not using Sharex Uploader`})
+        }
+        let mimeFile = mimeType.extension(files.cyci.mimetype);
+        //const mimeFile = files.cyci.name.substr(files.cyci.name.lastIndexOf('.') + 1, files.cyci.name.length).toLowerCase();
+        if (!fileSettings.extensions.includes(mimeFile)) return res.json({error: `Invalid mime-type`});
+
+
+        let file = files.cyci;
+        let fileType = file.type;
+        let fileSize = file.size;
+        let fileName = file.originalFilename;
+       
+        // let filePath = `${fileSettings.filePath}/${fileName}`;
+        // let fileURL = `${fileSettings.fileURL}/${fileName}`;
+        
+        let fileData = {
+            fileName: fileName,
+            fileType: fileType,
+            fileSize: fileSize,
+            fileExtension: files.cyci.originalFilename.substring(file.originalFilename.lastIndexOf('.') + 1, file.originalFilename.length).toLowerCase()
+        };
+        //console.log(fileData);
+
+        //let fileUpload = await s3A.uploadFile(fileData);
+        //console.log(fileData);
+    });
     
     //
     //// models.User.addImageOrFile(account, {name: "fileName", value: "fileName", size: "fileSize", type: "fileType", created_at: "now"});
@@ -73,7 +111,7 @@ async function post(req, res) {
     //     });
     // });
     // return;
-    return res.json({error: 'Not implemented'});
+    //return res.json({error: 'Not implemented'});
 }
 
 module.exports = { post };
