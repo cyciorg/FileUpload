f51d167cb78d5a935d7227752c4b582b16b7f781
Phil - DopeDealers <dopedealersdev@gmail.com>
2022-11-27 23:20:03 -0500

    hehe

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -18,9 +18,6 @@ async function post(req, res) {
     let account = await models.User.findByEmailOrId({ email: userHeaderInformation['x-user-email'], userid: userHeaderInformation['x-user-id'] }).then();
     if (account.is_banned) return res.json({error: `Unauthorized request. user is banned.`});
     if ((account instanceof Error)) return res.json({error: `Unauthorized request. user does not exist.`}); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
-    console.log(userHeaderInformation['x-user-api-token'])
-    console.log(req.headers)
-
     if (account.api_token !== userHeaderInformation['x-user-api-token']) return res.json({ error: `Unauthorized request. user api token is invalid.` }); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
     //if (!account.api_token || account.api_token !== userHeaderInformation['x-user-api_token']) return res.json({ error: `Unauthorized request. User does not have an api token. Or api token does not match.` }); // logger.error(`Unauthorized request to /upload/ by ${ip} - ${who}`),
     
