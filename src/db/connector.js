6e4017822f759cbcac0330991ed2836bed71b725
DopeDealers <admin@cyci.org>
2023-05-12 00:13:17 -0400

    Update connector.js

--- src/db/connector.js
+++ src/db/connector.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const User = require('./User.schema.js');
 
-const connectDb = () => {
-  return mongoose.connect(process.env.MONGO_URI);
+const connectDb = async () => {
+  return await mongoose.connect(process.env.MONGO_URI);
 };
 
 
